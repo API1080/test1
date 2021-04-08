ea6ac1b747ffc2ef56dbd3337e1782971d38b226
API1080 <aniket.mandal@apisero.com>
2021-04-08 14:38:35 +0530

    commit 1

--- bbandgit.java
+++ bbandgit.java
@@ -3,6 +3,6 @@ class Demo {
                 System.out.println("local to BitBucker and github -> SUCCESS");
                 System.out.println("SUCCESSFULLY COMPLETED");
                 System.out.println("Auto commit message");
-               
+                System.out.println("Auto commit message 2");
 	}
 }
